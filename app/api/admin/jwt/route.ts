d55f74fb49f98bd7b9ea2015144bac109bd89552
irfansud2nd <irfansud2nd@gmail.com>
2024-03-26 16:03:36 +0700

    jwt route

--- app/api/admin/jwt/route.ts
+++ app/api/admin/jwt/route.ts
@@ -3,9 +3,7 @@ import { NextResponse } from "next/server";
 
 export const POST = async (req: Request) => {
   const { email } = await req.json();
-  const token = sign(
-    { email, isAdmin: true },
-    process.env.JWT_SECRET as string
-  );
+  const JWT_SECRET = process.env.JWT_SECRET as string;
+  const token = sign({ email, isAdmin: true }, JWT_SECRET);
   return NextResponse.json({ token }, { status: 200 });
 };
