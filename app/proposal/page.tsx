a4bd72e485ffce8a9e382687c1595f3da881f262
irfansud2nd <irfansud2nd@gmail.com>
2024-04-18 19:42:13 +0700

    update proposal link

--- app/proposal/page.tsx
+++ app/proposal/page.tsx
@@ -2,7 +2,7 @@ import { redirect } from "next/navigation";
 
 const page = () => {
   redirect(
-    "https://firebasestorage.googleapis.com/v0/b/ladwitra-2024.appspot.com/o/proposal%2FProposal%20Kejuaraan%20La%20Dwitra%202024.pdf?alt=media&token=9b3d59de-b529-4237-beed-8dff15ce241f"
+    "https://firebasestorage.googleapis.com/v0/b/ladwitra-2024.appspot.com/o/proposal%2FProposal%20Kejuaraan%20La%20Dwitra%202024.pdf?alt=media&token=525362cb-5551-440c-b30d-28e87963d8fc"
   );
 };
 export default page;
